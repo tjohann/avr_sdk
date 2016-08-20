ba288ce115b64efc660f026297c0d483c0f1dc60
Thorsten Johannvorderbrueggen <myhalfdan@googlemail.com>
2016-08-20 14:49:58 +0200

    add first step to add arduino nano 3 as bare-metal device

---
+++ include/defines_m328p.mk
@@ -0,0 +1,32 @@
+# common defines for m328
+
+# arduino default fuses
+# low_fuses=0xff
+# high_fuses=0xde
+# extended_fuses=0x05
+
+
+# atmege368 defaults
+LFUSE_DEF = 0x62
+# full-speed-mode -> CLDIV8 = 1
+# -> LFUSE = 0xE2
+HFUSE_DEF = 0xdf
+# preserve eeprom across flashing
+# -> HFUSE = 0xD7
+EFUSE_DEF = 0x00
+
+# atmega168 project specific
+LFUSE = 0x62
+HFUSE = 0xdf
+EFUSE = 0x00
+
+# default fuses
+FUSE_DEFAULT = -U lfuse:w:$(LFUSE_DEF):m -U hfuse:w:$(HFUSE_DEF):m -U efuse:w:$(EFUSE_DEF):m
+
+#
+# Note:
+#       set also FUSE_STRING in project makefile
+#
+# to add some useful changes set the below values
+# full-speed-mode -> CLDIV8 = 1 -> LFUSE = 0xE2
+# preserve eeprom across flashing -> HFUSE = 0xD7
